688c4a3f3d58ed7eae88e7996bddeb2a9e73c6fe
Laurie Chang <lauriechang05@gmail.com>
2024-02-27 11:51:22 -0800

    two more class backups

---
+++ class04/class04.R
@@ -0,0 +1,9 @@
+# my first R script
+
+x <- 1:50
+plot(x, sin(x))
+plot(x, sin(x), typ="l")
+plot(x, sin(x), typ="l", col="pink")
+plot(x, sin(x), typ="l", col="pink", lwd=2)
+plot(x, sin(x), typ="l", col="pink", lwd=2, xlab="hello")
+
